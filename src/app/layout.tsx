d032a70b999d75b8353708067f4bb72938d07cca
Andrew Schreiber <andrew.schreiber1@gmail.com>
2024-07-20 15:08:45 -0400

    feat: google analytics base integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { cn } from "@/lib/utils";
+import Script from 'next/script'
+
 
 export const metadata: Metadata = {
   title: "Highcast",
@@ -26,6 +28,22 @@ export default function RootLayout({ children }: RootLayoutProps) {
       )}
       lang="en"
     >
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DKY5RF2R8B"
+        />
+
+        <Script id="google-analytics">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-DKY5RF2R8B');
+        `}
+        </Script>
+      </head>
+
       <body>
         <main className="font-sans antialiased">{children}</main>
       </body>
